d4e6ae4e459e6f7733f0b4be404581fd074ec100
jkang4531 <jkang4531@naver.com>
2022-07-26 14:40:10 +0900

    Coin Tracker
    Api를 받아와서 환전해주는 시스템

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  const [money, setMoney] = useState(0);
+  const [selected, setSelected] = useState();
+
+  const onChange = (event) => {
+    setMoney(event.target.value);
+    console.log(event.target.value);
+  };
+  const onSelect = (event) => {
+    setSelected(event.target.value);
+  };
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
+  console.log(typeof money);
+  console.log(typeof selected);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
+      {loading ? (
+        <strong>Loading...</strong>
+      ) : (
+        <div>
+          <select onChange={onSelect} value={selected}>
+            {coins.map((coin) => (
+              <option value={coin.quotes.USD.price}>
+                {coin.name}({coin.symbol}): ${coin.quotes.USD.price} USD
+              </option>
+            ))}
+          </select>
+          <input
+            onChange={onChange}
+            value={money}
+            type="number"
+            placeholder="환전할 금액을 알려주세요"
+          />
+          <h3>{money / selected}</h3>
+        </div>
+      )}
     </div>
   );
 }
